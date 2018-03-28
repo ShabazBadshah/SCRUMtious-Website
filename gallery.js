daf9a24ba13e4b5fdd427aae790d11b6aaf8be26
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-03-28 01:17:20 -0400

    minified all image that were being used which took up the majority of the site size, site is now down to 2.66MB from 8MB

--- gallery.js
+++ gallery.js
@@ -4,14 +4,14 @@ const IMAGE_TYPE = ".png";
 var galleryItem;
 
 var appGalleryImageSrc = [
-    ROOT_IMG_DIR + "app-splash-screen" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-overview" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-sprint-list-screen" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-stats-screen" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-user-stories-screen" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-group-chat" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-task-in-progress-screen" + IMAGE_TYPE,
-    ROOT_IMG_DIR + "project-task-completed-screen" + IMAGE_TYPE
+    ROOT_IMG_DIR + "app-splash-screen-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-overview-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-sprint-list-screen-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-stats-screen-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-user-stories-screen-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-group-chat-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-task-in-progress-screen-min" + IMAGE_TYPE,
+    ROOT_IMG_DIR + "project-task-completed-screen-min" + IMAGE_TYPE
 ];
 
 $(function() {
